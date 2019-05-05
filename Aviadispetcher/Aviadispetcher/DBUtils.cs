fd74118238adf1e0ae07cdb1630d7a15f95e76a3
Vadim <vadim508@ukr.net>
2019-05-05 15:11:34 +0300

    Add try catch

--- Aviadispetcher/Aviadispetcher/DBUtils.cs
+++ Aviadispetcher/Aviadispetcher/DBUtils.cs
@@ -4,37 +4,50 @@ using System.Linq;
 using System.Text;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+//using System.Windows.Forms;
+using System.Windows;
 //...
 
 namespace Aviadispetcher
 {
     class DBUtils
     {
-        //...
+
         public static void OpenDbFile(List<Flight> fList)
         {
-            //рядок з'єднання з БД
-            string connStr = "Server = localhost; Database = aviadispetcher; Uid = root; Pwd = ;";
-            MySqlConnection conn = new MySqlConnection(connStr);
-            MySqlCommand command = new MySqlCommand();
-            //запис вибору даних із БД
-            string commandString = "SELECT * FROM rozklad;";
-            command.CommandText = commandString;
-            command.Connection = conn;
-            //читання відповіді сервера
-            MySqlDataReader reader;
-            command.Connection.Open();
-            reader = command.ExecuteReader();
-            //запис зчитаних даних у масив та список для виведення на екран
-            int i = 0;
-            while(reader.Read())
+            try
             {
-                //формувати запис про рейс, зчитаного із БД
-                fList.Add(new Flight((string)reader["Number"], (string)reader["City"], (string) reader["DepatureTime"],
-                            (string)reader["FreeSeats"]));
+                //рядок з'єднання з БД
+                string connStr = "Server = localhost; Database = aviadispetcher; Uid = root; Pwd = ;";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                MySqlCommand command = new MySqlCommand();
+                //запис вибору даних із БД
+                string commandString = "SELECT * FROM rozklad;";
+                command.CommandText = commandString;
+                command.Connection = conn;
+                //читання відповіді сервера
+                MySqlDataReader reader;
+                command.Connection.Open();
+                reader = command.ExecuteReader();
+                //запис зчитаних даних у масив та список для виведення на екран
+                int i = 0;
+                while (reader.Read())
+                {
+                    //формувати запис про рейс, зчитаного із БД
+                    fList.Add(new Flight((string)reader["Number"], (string)reader["City"], (string)reader["DepatureTime"],
+                                (string)reader["FreeSeats"]));
+                }
+                reader.Close();
             }
-            reader.Close();
+            catch (Exception ex)
+            {
+                MessageBox.Show((ex.Message + char.ConvertFromUtf32(13)
+                        + char.ConvertFromUtf32(13) + "Для завантаження файлу" +
+                        "виконайте команду Файл-Завантажити"), "Помилка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
         }
-        //...
+       
     }
 }
