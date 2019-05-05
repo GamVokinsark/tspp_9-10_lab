3d906d5570a38286ed224c8c4095ccb397070e42
Vadim <vadim508@ukr.net>
2019-05-05 14:32:23 +0300

    add WordUtils and DBUtils

--- Aviadispetcher/Aviadispetcher/DBUtils.cs
+++ Aviadispetcher/Aviadispetcher/DBUtils.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 //...
 
 namespace Aviadispetcher
@@ -9,9 +11,29 @@ namespace Aviadispetcher
     class DBUtils
     {
         //...
-        public static void OpenDbFile()
+        public static void OpenDbFile(List<Flight> fList)
         {
-            //...
+            //рядок з'єднання з БД
+            string connStr = "Server = localhost; Database = aviadispetcher; Uid = root; Pwd = ;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            MySqlCommand command = new MySqlCommand();
+            //запис вибору даних із БД
+            string commandString = "SELECT * FROM rozklad;";
+            command.CommandText = commandString;
+            command.Connection = conn;
+            //читання відповіді сервера
+            MySqlDataReader reader;
+            command.Connection.Open();
+            reader = command.ExecuteReader();
+            //запис зчитаних даних у масив та список для виведення на екран
+            int i = 0;
+            while(reader.Read())
+            {
+                //формувати запис про рейс, зчитаного із БД
+                fList.Add(new Flight((string)reader["Number"], (string)reader["City"], (string) reader["DepatureTime"],
+                            (string)reader["FreeSeats"]));
+            }
+            reader.Close();
         }
         //...
     }
--- Aviadispetcher/Aviadispetcher/WordUtils.cs
+++ Aviadispetcher/Aviadispetcher/WordUtils.cs
@@ -2,10 +2,151 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Office.Interop.Word;
+using abit = System.Windows.Forms;
+using F = System.Windows.Forms;
+
 
 namespace Aviadispetcher
 {
     class WordUtils
     {
+        private Application app;
+        private Document document;
+        private object missing = System.Reflection.Missing.Value;
+
+        /// <summary>
+        /// Конструктор для ініціалізації.
+        /// </summary>
+        public WordUtils()
+        {
+            app = new Application();
+            //app.ShowAnimation = false;
+            app.Visible = false;
+            document = app.Documents.Add(ref missing,
+                                ref missing, ref missing, ref missing);
+        }
+
+        /// <summary>
+        /// Додає заголовок файлу.
+        /// </summary>
+
+        public void AddHeader()
+        {
+            var pText1 = document.Paragraphs.Add();
+            pText1.Range.Text = "Звіт";
+            pText1.Range.Bold = 1;
+            pText1.Range.Font.Size = 20;
+            pText1.Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+            pText1.Range.InsertParagraphAfter();
+        }
+        /// <summary>
+        /// Додає абзац 
+        /// </summary>
+
+        public void AddParagraphs(String text)
+        {
+            var pText = document.Paragraphs.Add();
+            pText.Range.Text = text;
+            pText.Range.Bold = 0;
+            pText.Range.Font.Size = 14;
+            pText.Range.Font.Name = "Times New Roman";
+            pText.Format.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
+            pText.Range.InsertParagraphAfter();
+        }
+        /// <summary>
+        /// Генерує заголовок таблиці.
+        /// </summary>
+        private void AddStringToColmn(Cell cell, String text)
+        {
+            cell.Range.Text = text;
+            cell.Range.Font.Bold = 1;
+            //other format properties goes here  
+            cell.Range.Font.Name = "verdana";
+            cell.Range.Font.Size = 14;
+            //cell.Range.Font.ColorIndex = WdColorIndex.wdGray25;                              
+            cell.Shading.BackgroundPatternColor = WdColor.wdColorGray25;
+            //Center alignment for the Header cells  
+            cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+            cell.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+        }
+
+        /// <summary>
+        /// Генерує таблицю з даними про абітурієнтів.
+        /// </summary>
+        public void AddTable(abit.BindingSource result)
+        {
+            Paragraph para = document.Content.Paragraphs.Add(ref missing);
+            Table firstTable = document.Tables.Add(para.Range, (int)result.Count + 1, 3, ref missing, ref missing);
+            String[] Coulmn = new String[] { "Прізвище", "І'мя", "Оцінка" };
+
+            firstTable.Borders.Enable = 1;
+            foreach (Row row in firstTable.Rows)
+            {
+                foreach (Cell cell in row.Cells)
+                {
+                    //Header row  
+                    if (cell.RowIndex == 1)
+                    {
+                        switch (cell.ColumnIndex)
+                        {
+                            case 1:
+                                AddStringToColmn(cell, Coulmn[0]);
+                                break;
+                            case 2:
+                                AddStringToColmn(cell, Coulmn[1]);
+                                break;
+                            case 3:
+                                AddStringToColmn(cell, Coulmn[2]);
+                                break;
+                        }
+
+                    }
+                    //Data row  
+                    else
+                    {
+                        switch (cell.ColumnIndex)
+                        {
+                            case 1:
+                                cell.Range.Text = ((AbitResult)result[cell.RowIndex - 2]).Surname;
+                                break;
+                            case 2:
+                                cell.Range.Text = ((AbitResult)result[cell.RowIndex - 2]).Name;
+                                break;
+                            case 3:
+                                cell.Range.Text = ((AbitResult)result[cell.RowIndex - 2]).Mark.ToString();
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Зберігає Word файл до обраної папки.
+        /// </summary>
+        public void Save()
+        {
+            F.SaveFileDialog dlg = new F.SaveFileDialog
+            {
+                FileName = "Zvit", // Default file name
+                DefaultExt = ".doc", // Default file extension
+                Filter = "Text documents (.doc)|*.doc" // Filter files by extension
+            };
+            dlg.ShowDialog();
+            if (dlg.FileName != "Zvit")
+            {
+                document.SaveAs(@dlg.FileName);
+                //F.MessageBox.Show(dlg.FileName, "Помилка");
+            }
+        }
+        /// <summary>
+        /// Закриває Word програму.
+        /// </summary>
+        public void Close()
+        {
+            document.Close(WdSaveOptions.wdDoNotSaveChanges);
+            app.Quit();
+        }
     }
+
 }
